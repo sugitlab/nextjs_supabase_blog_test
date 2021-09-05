ccb9618fa1bc6662ffd5a5c1ebabe229e20bce9f
Sugit <sgmt.snj@gmail.com>
2021-09-05 22:53:00 +0900

    add passHref to Link component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,17 +26,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <nav className="p-6 border-b border-gray-300">
-        <Link href="/">
+        <Link passHref href="/">
           <span className="mr-6 cursor-pointer">Home</span>
         </Link>
         {
           user && (
-            <Link href="/create-post">
+            <Link passHref href="/create-post">
               <span className="mr-6 cursor-pointer">Create Post</span>
             </Link>
           )
         }
-        <Link href="/profile">
+        <Link passHref href="/profile">
           <span className="mr-6 cursor-pointer">Profile</span>
         </Link>
       </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       <h1 className = "text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
       {
         posts.map(post => (
-          <Link key={post.id} href={`/posts/${post.id}`}>
+          <Link key={post.id} passHref href={`/posts/${post.id}`}>
             <div className = "cursor-pointer border-b border-gray-300 mt-8 pb-4">
               <h2 className = "text-xl font-semibold">{post.title}</h2>
               <p className = "text-gray-500 mt-2">Author: {post.user_email}</p>
